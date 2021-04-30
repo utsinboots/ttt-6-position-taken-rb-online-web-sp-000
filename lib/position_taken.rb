a27588d648de0c5774802c5ec99d4211a2a3901b
Utshant Gurung <utshantgurung@gmail.com>
2021-04-30 09:16:14 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,7 @@
 def position_taken?(board, index)
   if board[index] == "" || board[index] ==" " || board[index] == nil
     false
+  elsif board[index] == "X" || board[index] == "O"
+    true
   end
-    
 end
